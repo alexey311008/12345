897e522a93ef25ce885e4994648a8b6e57ca6df9
Alan <95705529+Alanumberone@users.noreply.github.com>
2024-03-30 09:17:16 +0100

    added disappearance of objects on press dice.js
    next step is to make the next page and make it appear on button press

---
+++ project_dice/dice.js
@@ -0,0 +1,10 @@
+let box = document.querySelector(".box")
+let startButton = document.querySelector(".start-button")
+let header = document.querySelector(".header")
+
+startButton.addEventListener("click",function(){
+    box.style.display="none"
+    header.style.display="none"
+    startButton.style.display="none"
+
+})
